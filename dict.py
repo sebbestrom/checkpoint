8a68185d5e89c0b872006cf8a32665fef2da8d17
sebbestrom <stroom42@gmail.com>
2022-11-25 15:37:28 +0100

    changes made

--- dict.py
+++ dict.py
@@ -5,17 +5,19 @@ conn = psycopg2.connect(
    user="postgres",
    password="sudden21"
 )
-print("Use the commands: list, add, delete or quit")
+print("Use the commands: list, add, delete or quit") #prints the commands
 def read_dict(C):
     cur = C.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
+#add word
 def add_word(C, word, translation):
     cur = C.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
+#delete word
 def delete_word(C, ID):
     cur = C.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
